206832e28b22c5735b30eeec075ac28deb1ef4e5
Renukanarayan <renukagorle143@gmail.com>
2025-06-12 17:09:46 +0530

    Chat bot

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import './App.css';
 
-import './App.css'
-import Login from './components/loginrightsidebar'
+import Login from './components/loginrightsidebar';
+import ChatWidgetLauncher from './components/Button';
 
 function App() {
-
   return (
-    <>
-    <Login/>
-    </>
-  )
+    <div className="min-h-screen bg-gray-100 relative">
+      {/* Login sidebar or main UI */}
+      <Login />
+
+      {/* Floating Chatbot launcher */}
+      <ChatWidgetLauncher />
+    </div>
+  );
 }
 
-export default App
+export default App;
