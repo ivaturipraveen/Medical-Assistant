185cd9b0d2c21aeafe20c8dbb88656858330b4ce
Renukanarayan <renukagorle143@gmail.com>
2025-06-17 10:22:09 +0530

    components update1..

--- src/components/Specializations.tsx
+++ src/components/Specializations.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { FaBone } from 'react-icons/fa';
 import { GiBrain } from 'react-icons/gi';
 import { MdScience } from 'react-icons/md';
